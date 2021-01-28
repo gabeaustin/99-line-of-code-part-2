ca2531163699243a074bdc9cbaf9b1509f82af5c
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-01-28 08:56:16 -0600

    added code to have the lines of the song display when user clicks button

--- app.js
+++ app.js
@@ -14,29 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
         let h3Text = document.createTextNode("Friend 1 - Test");
         h3.appendChild(h3Text);
         document.body.appendChild(h3);
+
+        let friends = ["Jon", "Jamarcy", "B-Ry", "Gabe", "M. Little"];
+
+        // Outer loop to pick the friend
+        for (let i = 0; i < friends.length; i++) {
+            console.log(friends[i].toUpperCase() + ":");
+
+            let lines_of_code = 99;
+            // Inner loop to iterate through the lyrics
+            while (lines_of_code >= 2) {
+                console.log(lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friends[i] + " strikes one out, clears it all out...");
+                lines_of_code--;
+            }
+
+            // This is the code for when there is only 1 line of code left
+            console.log(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+        }
     })
-
-
-//     button.addEventListener("click", function() {
-//         let friends = ["Jon", "Jamarcy", "B-Ry", "Gabe", "M. Little"];
-
-//         // Outer loop to pick the friend
-//         for (let i = 0; i < friends.length; i++) {
-//             console.log(friends[i].toUpperCase() + ":");
-
-//             let lines_of_code = 99;
-//             // Inner loop to iterate through the lyrics
-//             while (lines_of_code >= 2) {
-//                 console.log(lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friends[i] + " strikes one out, clears it all out...");
-//                 lines_of_code--;
-//             }
-
-//             // This is the code for when there is only 1 line of code left
-//             console.log(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
-//         }
-// }
 })
-
-// )
-
 
