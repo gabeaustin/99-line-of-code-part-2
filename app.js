f766cd88cf67774831160f2144d5f564b8fdb6e0
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-01-28 10:51:35 -0600

    added code to loop through friends and print lyrics for the entire array of friends

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let divToHoldButton = document.createElement("div");
-    let button = document.createElement("button");
-    let buttonText = document.createTextNode("Sing!");
+document.addEventListener("DOMContentLoaded", function() { // line 35
+    let divToHoldButton = document.createElement("div"); // created a div
+    let button = document.createElement("button"); // put button in div
+    let buttonText = document.createTextNode("Sing!"); // changed text of button
 
-    document.body.appendChild(divToHoldButton);
-    divToHoldButton.appendChild(button);
-    divToHoldButton.style.border = "1px solid black";
-    button.appendChild(buttonText);
+    document.body.appendChild(divToHoldButton); // allows div to display on screen
+    divToHoldButton.appendChild(button); // allows button to display on screen
+    divToHoldButton.style.border = "1px solid black"; // for testing only
+    button.appendChild(buttonText); // allowed changed text of button to show
 
 
-    button.addEventListener("click", function() {
-        let h3 = document.createElement("h3");
-        let h3Text = document.createTextNode("Friend 1 - Test");
-        h3.appendChild(h3Text);
-        document.body.appendChild(h3);
+    button.addEventListener("click", function() { // line 34
 
-        let friends = ["Jon", "Jamarcy", "B-Ry", "Gabe", "M. Little"];
+    //     let h3 = document.createElement("h3");
+    //     let h3Text = document.createTextNode("Friend 1 - Test"); // for testing only
+    //     h3.appendChild(h3Text);
+    //     document.body.appendChild(h3);
+
+    let friends = ["Jon", "Jamarcy", "B-Ry", "Gabe", "M. Little"];
 
         // Outer loop to pick the friend
-        for (let i = 0; i < friends.length; i++) {
+        for (let i = 0; i < friends.length; i++) { // line 33
             console.log(friends[i].toUpperCase() + ":");
 
-            let lines_of_code = 99;
-            // Inner loop to iterate through the lyrics
-            while (lines_of_code >= 2) {
-                console.log(lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friends[i] + " strikes one out, clears it all out...");
-                lines_of_code--;
-            }
-
-            // This is the code for when there is only 1 line of code left
+        let lines_of_code = 99;
+        // Inner loop to iterate through the lyrics
+        while (lines_of_code >= 2) { // line 29
+            console.log(lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friends[i] + " strikes one out, clears it all out...");
+            lines_of_code--;
+    }
+        // This is the code for when there is only 1 line of code left
             console.log(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
         }
     })
