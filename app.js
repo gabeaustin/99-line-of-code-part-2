8cc90c8258c5d05f49f88b03821031b4974913d4
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-01 09:03:19 -0600

    continued to work on getting the lyrics on the screen

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const friendsArray = ["Jon", "Jamarcy", "B-Ry", "Gabe", "M. Little"];
 document.addEventListener("DOMContentLoaded", function () {
     const divToHoldButton = document.createElement("div"); // created a div
     const button = document.createElement("button"); // put button in div
-    const buttonText = document.createTextNode("Sing!");    
+    const buttonText = document.createTextNode("Sing!");
 
     document.body.appendChild(divToHoldButton); // allows div to display on screen
     divToHoldButton.appendChild(button); // allows button to display on screen
@@ -11,36 +11,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     button.addEventListener("click", function () {
-    // Outer loop to pick the friend
-    for (let i = 0; i < friendsArray.length; i++) {
-        const divToHoldFriends = document.createElement("div");
-        const h3WithFriendsName = document.createElement("h3");
-        const displayNamesOnScreen = document.createTextNode(friendsArray[i]); // keep
-        const paraToHoldLyrics = document.createElement("p");
-        const nameTitlesUppercase = document.createTextNode(friendsArray[i].toUpperCase() + ":");
+        // Outer loop to pick the friend
+        for (let i = 0; i < friendsArray.length; i++) {
+            const divToHoldFriends = document.createElement("div");
+            const h3WithFriendsName = document.createElement("h3");
+            const nameTitlesUppercase = document.createTextNode(friendsArray[i].toUpperCase() + ":");
 
-        divToHoldFriends.className = "friend";
-    
-        document.body.appendChild(divToHoldFriends);
-        h3WithFriendsName.appendChild(nameTitlesUppercase);
-        divToHoldFriends.appendChild(h3WithFriendsName);
-        
+            divToHoldFriends.className = "friend";
 
-        let lines_of_code = 99;
-        // Inner loop to iterate through the lyrics
-        while (lines_of_code >= 2) {
-            console.log(lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out...");
-            lines_of_code--;
+            document.body.appendChild(divToHoldFriends);
+            h3WithFriendsName.appendChild(nameTitlesUppercase);
+            divToHoldFriends.appendChild(h3WithFriendsName);
         }
-        // This is the code for when there is only 1 line of code left
-        console.log(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
-        let songVerses = document.createTextNode(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
-    }
-    
-    for (let j = 0; j < friendsArray.length; j++) {
-        // const displayNamesOnScreen = h3WithFriendsName.innerHTML = friendsArray[j]; // keep
-        // console.log(displayNamesOnScreen);
-        // const displayNamesOnScreen = document.createTextNode(friendsArray[j]);
+
+        for (let j = 0; j < friendsArray.length; j++) {
+            let lines_of_code = 99;
+
+            // Inner loop to iterate through the lyrics
+            while (lines_of_code >= 2) {
+                const paraToHoldLyrics = document.createElement("p");
+                const pluralSongVerses = (lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friendsArray[j] + " strikes one out, clears it all out...");
+                const singularSongVerses = (lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friendsArray[j] + " strikes one out, clears it all out, no more lines of code in the file");
+                const displayNamesOnScreen = document.createTextNode(friendsArray[i]); // keep
+
+                paraToHoldLyrics.appendChild(pluralSongVerses);
+                paraToHoldLyrics.appendChild(singularSongVerses);
+                divToHoldFriends.appendChild(paraToHoldLyrics);
+                lines_of_code--;
+            }
+
+            document.appendChild(divToHoldFriends);
+            displayNamesOnScreen.appendChild(paraToHoldLyrics);
+            paraToHoldLyrics.appendChild(singularSongVerses);
         }
     })
 })
