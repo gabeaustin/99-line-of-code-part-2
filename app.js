73604bdd4ee915d9ef2d51ebc508504474ebcdcb
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-01 13:14:14 -0600

    worked on bug fixes - added code so the lyrics show on the screen

--- app.js
+++ app.js
@@ -16,34 +16,39 @@ document.addEventListener("DOMContentLoaded", function () {
             const divToHoldFriends = document.createElement("div");
             const h3WithFriendsName = document.createElement("h3");
             const nameTitlesUppercase = document.createTextNode(friendsArray[i].toUpperCase() + ":");
+            const displayNamesOnScreen = document.createTextNode(friendsArray[i]); // keep
 
             divToHoldFriends.className = "friend";
 
             document.body.appendChild(divToHoldFriends);
             h3WithFriendsName.appendChild(nameTitlesUppercase);
             divToHoldFriends.appendChild(h3WithFriendsName);
-        }
 
-        for (let j = 0; j < friendsArray.length; j++) {
             let lines_of_code = 99;
 
             // Inner loop to iterate through the lyrics
             while (lines_of_code >= 2) {
                 const paraToHoldLyrics = document.createElement("p");
-                const pluralSongVerses = (lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friendsArray[j] + " strikes one out, clears it all out...");
-                const singularSongVerses = (lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friendsArray[j] + " strikes one out, clears it all out, no more lines of code in the file");
-                const displayNamesOnScreen = document.createTextNode(friendsArray[i]); // keep
+                const pluralSongVerses = (lines_of_code + " lines of code in the file, " + (lines_of_code - 1) + " lines of code; " + friendsArray[i] + " strikes one out, clears it all out...");
+                const singularSongVerses = (lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
 
-                paraToHoldLyrics.appendChild(pluralSongVerses);
-                paraToHoldLyrics.appendChild(singularSongVerses);
-                divToHoldFriends.appendChild(paraToHoldLyrics);
+                paraToHoldLyrics.append(singularSongVerses);
+                paraToHoldLyrics.append(pluralSongVerses);
+                divToHoldFriends.append(paraToHoldLyrics);
                 lines_of_code--;
             }
-
-            document.appendChild(divToHoldFriends);
-            displayNamesOnScreen.appendChild(paraToHoldLyrics);
-            paraToHoldLyrics.appendChild(singularSongVerses);
+            
+            const paraToHoldLyrics = document.createElement("p");
+            const singularSongVerses = (lines_of_code + " line of code in the file, " + lines_of_code + " line of code; " + friendsArray[i] + " strikes one out, clears it all out, no more lines of code in the file");
+                
+            document.body.appendChild(divToHoldFriends);
+            divToHoldFriends.append(paraToHoldLyrics);
+            paraToHoldLyrics.append(singularSongVerses);
         }
+
+        
+
+        
     })
 })
 
